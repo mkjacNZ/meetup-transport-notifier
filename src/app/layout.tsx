6683a7dd00190530685fc5254e8019e1f585a831
Mel <mkjacobsonnz+port@gmail.com>
2023-05-26 19:58:53 +1200

    google maps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use-client"
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -13,9 +14,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/app/map.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import React, { Component } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { decode, encode } from "@googlemaps/polyline-codec";
+
+export default async function Map() {
+  const containerStyle = {
+    width: '1000px',
+    height: '600px'
+  };
+
+  // Auckland Art Gallery
+  let startPointLat = -36.851068176829905;
+  let startPointLng = 174.76628605352832;
+
+  // Sky Tower
+  let endPointLat = -36.84812374907156;
+  let endPointLng = 174.76233102141367;
+
+  const startPosition = { lat: startPointLat, lng: startPointLng }
+  const endPosition = { lat: endPointLat, lng: endPointLng }
+
+  const center = {
+    lat: ((startPointLat + endPointLat) / 2.0),
+    lng: ((startPointLng + endPointLng) / 2.0)
+  }
+
+  console.log(center.lat + ' ' + center.lng);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline',
+    } as Record<string, string>,
+    body: JSON.stringify({
+      "origin": {
+        "location": {
+          "latLng": {
+            "latitude": startPointLat,
+            "longitude": startPointLng,
+          }
+        }
+      },
+      "destination": {
+        "location": {
+          "latLng": {
+            "latitude": endPointLat,
+            "longitude": endPointLng,
+          }
+        }
+      },
+      "travelMode": "DRIVE",
+      "routingPreference": "TRAFFIC_AWARE",
+      "departureTime": "2023-10-15T15:01:23.045123456Z",
+      "computeAlternativeRoutes": false,
+      "routeModifiers": {
+        "avoidTolls": false,
+        "avoidHighways": false,
+        "avoidFerries": false
+      }
+    }),
+  };
+
+  let data: any = {};
+
+  try {
+    const response = await fetch('https://routes.googleapis.com/directions/v2:computeRoutes', requestOptions);
+    data = await response.json()
+
+    // Handle the response
+    console.log(data.routes[0]);
+    console.log(decode(data.routes[0].polyline.encodedPolyline, 5));
+  } catch {
+    // Handle any errors
+    console.log('error');
+  }
+
+  return (
+    <LoadScript
+      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={14}
+      >
+        <Marker
+          position={startPosition}
+          title='Start'
+        />
+        <Marker
+          position={endPosition}
+          title='End'
+        />
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
+}
